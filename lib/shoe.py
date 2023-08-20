1623d94095e4a764630dfaf9b05b294a60d20f4c
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-20 22:43:57 +0300

    completed challenge1

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size  # Use an underscore to indicate it's a private attribute
+        self.condition = "Used"
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        self.condition = "New"
+
+    def __str__(self):
+        return f"Brand: {self.brand}, Size: {self.size}, Condition: {self.condition}"
