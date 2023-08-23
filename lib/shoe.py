a49ce613747cd9097c7a745c34dce2665eca0bc2
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-23 12:56:42 +0300

    completed lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,22 +1,17 @@
+#!/usr/bin/env python3
+
 class Shoe:
     def __init__(self, brand, size):
         self.brand = brand
-        self._size = size  # Use an underscore to indicate it's a private attribute
-        self.condition = "Used"
-
-    @property
-    def size(self):
-        return self._size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            print("size must be an integer")
+        self.size = size
+    
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
         else:
-            self._size = value
-
-    def cobble(self):
-        self.condition = "New"
-
-    def __str__(self):
-        return f"Brand: {self.brand}, Size: {self.size}, Condition: {self.condition}"
+            print ("size must be an integer")
+            
+    def get_size(self):
+        return self._size
+    
+    size = property(get_size, set_size)
